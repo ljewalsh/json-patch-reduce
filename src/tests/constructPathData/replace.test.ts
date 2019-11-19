3733848f7998dcd43458b8448c6d6f7f50e4a4e4
Lucy-Jane Walsh <ljewalsh@gmail.com>
2019-11-19 11:40:44 -0500

    Remove test double-up

--- src/tests/constructPathData/replace.test.ts
+++ src/tests/constructPathData/replace.test.ts
@@ -78,30 +78,6 @@ test("returns expected logic when the patch includes an add then a replace opera
     t.deepEqual(pathValues, expectedPathValues)
 })
 
-test("returns expected logic when the patch includes an replace then a remove operation", (t) => {
-    const patch = [
-        {
-            op: REPLACE,
-            path: "/foo",
-            value: "baz",
-        } as ReplaceOperation,
-        {
-            op: REMOVE,
-            path: "/foo",
-        } as RemoveOperation,
-    ]
-
-    const { pathLogic, pathValues } = constructPathData(patch)
-
-    const expectedLogic = {
-        foo: REMOVE,
-    }
-    const expectedPathValues = {}
-
-    t.deepEqual(pathLogic, expectedLogic)
-    t.deepEqual(pathValues, expectedPathValues)
-})
-
 test("returns expected logic when the patch includes an add, replace, and then a remove operation", (t) => {
     const patch = [
         {
